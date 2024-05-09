529487175a9e95216d54e8355a9e3689e2df32b4
Harshvardhan Chauhan <harshvardhancc@gmail.com>
2024-05-08 23:39:23 -0400

    Create api service and component

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ProductsService } from '../services/products.service';
+import { Products } from '../../types';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  constructor(private productsService: ProductsService) {}
+
+  ngOnit() {
+    this.productsService
+      .getProducts('http://localhost:3000/clothes', { page: 1, perPage: 5 })
+      .subscribe((products: Products) => {
+        console.log(products.items);
+      });
+  }
+}
